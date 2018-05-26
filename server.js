44f38e821a70010c13f3327a3b2d2923369f4c77
DESKTOP-U51IVSG\abd <duongnk.techkids@gmail.com>
2018-05-26 19:26:39 +0700

    hmm...this should be work

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require('express');
 var path = require('path');
 var serveStatic = require('serve-static');
 let app = express();
-app.use(serveStatic(__dirname + "/dist"));
+app.use("/", serveStatic(path.join(__dirname + "/dist")));
 app.get('*', function (req, res) {
     res.sendFile(__dirname + '/index.html')
 })
