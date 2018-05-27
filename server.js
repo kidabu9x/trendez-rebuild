d40e54972f03f696a9273534564617f658833de4
DESKTOP-U51IVSG\abd <duongnk.techkids@gmail.com>
2018-05-27 09:30:11 +0700

    qweqweqweqweq

--- server.js
+++ server.js
@@ -4,7 +4,10 @@ const serveStatic = require('serve-static');
 const history = require('connect-history-api-fallback');
 let app = express();
 app.use(history())
-app.use(serveStatic(__dirname + '/dist'));
+app.use(serveStatic(path.join(__dirname + '/dist')));
+app.get('/', function (req, res) {
+    res.render(path.join(__dirname + '/dist/index.html'));
+});
 var port = process.env.PORT || 5000;
 app.listen(port);
 console.log('Server started '+ port);
